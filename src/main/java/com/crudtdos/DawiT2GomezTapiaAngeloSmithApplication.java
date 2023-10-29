a167acf076157eeb8c07b44efacc6073708d12a2
Angelo <gangelos630@gmail.com>
2023-10-28 21:38:33 -0500

    subida de repo del examen T2 DAWI

---
+++ src/main/java/com/crudtdos/DawiT2GomezTapiaAngeloSmithApplication.java
@@ -0,0 +1,13 @@
+package com.crudtdos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiT2GomezTapiaAngeloSmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiT2GomezTapiaAngeloSmithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudtdos/DawiT2GomezTapiaAngeloSmithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudtdos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiT2GomezTapiaAngeloSmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
